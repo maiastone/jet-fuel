4441081a5085e91f3165aa684ab0584a81c9a826
ccgamble <christine.c.gamble@gmail.com>
2017-01-24 16:21:50 -0700

    can post to folder, working on adding urls within folder

--- app.js
+++ app.js
@@ -1,21 +1,61 @@
-var express = require('express')
+var express = require('express');
+var jsdom = require("jsdom");
+var $ = require("jquery");
+var bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
+const md5 = require('md5')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', process.envPORT || 3000);
+
+let folders = [
+]
+
+app.locals.folders = {
+  0: 'folder one',
+  1: 'folder two'
+};
 
 app.get('/', (req, res) => {
-
 });
 
-app.post('/api/shorten', (req, res) => {
-
+app.get('/api/folders', (request, response) => {
+  response.json(app.locals.folders);
 });
 
-app.get('/:encoded_id', (req, res) => {
+app.post('/api/folders', (request, response) => {
+	const { folderName} = request.body;
+	const id = md5(folderName);
 
+  app.locals.folders[id] = folderName;
+  console.log(id, folderName);
+  response.json({ id, folderName})
+})
+
+app.get('/api/folders/:id', (request, response) => {
+  const {id} = request.params
+  const folder = app.locals.folders[id]
+
+  if(!folder){
+    response.sendStatus(404);
+  }
+  response.json({id, folder})
+})
+
+app.post('/api/folders/:id', (request, response) => {
+	const { url } = request.body;
+	const urlId = md5(url);
+
+	
 });
 
 app.listen(3000, () => {
   console.log('listening');
 });
+
+// $('.url-submit').click(function() {
+// 	alert('i was clicked')
+// })
