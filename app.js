4ea7bb8718dec4c8619734e2792ddc83115240f5
ccgamble <christine.c.gamble@gmail.com>
2017-01-26 08:19:08 -0700

    adds folders to dropdown and main page, can add url to specific folder, still need to render urls on homepage and within folder page, url info renders on api/url

--- app.js
+++ app.js
@@ -1,86 +1,89 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 const md5 = require('md5');
+const moment = require('moment');
+const shortid = require('shortid');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('port', process.envPORT || 3000);
 
-app.locals.folders = [
-  {
-    id: 1,
-		title: 'folder one'
-  },
-  {
-    id: 2,
-		title: 'folder two'
-  }
-]
-app.locals.urls = [
-  {
-    folderId: '1',
-    url: 'www.google.com',
-		shorturl: 0,
-    date: Date.now(),
-    clickCount: 0
-  }
-]
+app.set('port', process.env.PORT || 3000);
 
-app.get('/', (request, response) => {
-});
+app.locals.title = 'Jet Fuel'
+app.locals.folders = []
+app.locals.urls = []
+
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/api/folders', (request, response) => {
-  response.json(app.locals.folders);
-});
+  response.json(app.locals.folders)
+})
+
+app.get('/api/urls', (request, response) => {
+  response.json(app.locals.urls)
+})
 
 app.post('/api/folders', (request, response) => {
-	const { folderName } = request.body;
-	const folderID = md5(folderName);
 
-  app.locals.folders[folderID] = folderName;
-  console.log(folderID, folderName);
-  response.json({ folderID, folderName})
+  const { folder } = request.body
+  const id = md5(folder)
+
+  if (!folder) {
+   return response.status(422).send({
+     error: 'No folder provided'
+   });
+ }
+
+  app.locals.folders.push({ folder_title: folder, id: id})
+
+  response.status(201).json({
+      folder_title: folder,
+      id: id
+   })
 });
 
 app.get('/api/folders/:id', (request, response) => {
-  const {id} = request.params;
-	var result = app.locals.folders.filter(function(folder) {
-		return folder.id === parseInt(id)
-	})
-	console.log(app.locals.folders)
-  if(!result){
-    response.sendStatus(404);
-  }
-  response.json(result)
-});
+  const { id } = request.params;
+  const folder = app.locals.folders.filter(function(folder) { return fol.id === id })
 
-app.post('/api/folders/:folderID', (request, response) => {
-	const {folderID} = request.params;
-	const { url } = request.body;
-	const urlId = md5(url);
+  if(!folder) { return response.sendStatus(404); }
 
-	app.locals.urls[urlId] = {
-		folderID,
-		url,
-		shorturl: urlId,
-		date: Date.now(),
-		clickCount: 0
-	}
-	response.json(app.locals.urls)
-});
-
-app.get('/api/folders/:folderid/:shorturl', (request, response) => {
-  const {folderid, shorturl} = request.params
-  const url = app.locals.urls[shorturl]
-
-  response.json(url)
+  response.json({ folder })
 })
 
-app.listen(3000, () => {
-  console.log('listening');
+app.post('/api/urls', (request, response) => {
+  const { url, folderId } = request.body
+  const id = md5(url)
+  const short = 'http://' + shortid.generate()
+  const created = moment()
+
+  if (!url) {
+    return response.status(422).send({
+      error: 'No url provided'
+    });
+  }
+
+  app.locals.urls.push({
+    id: id,
+    folder_id: folderId,
+    short_url: short,
+    original_url: url,
+    created_at: created,
+  })
+
+  response.status(201).json({
+    id: id,
+    folder_id: folderId,
+    short_url: short,
+    original_url: url,
+    created_at: created,
+  })
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
 
 app.use(function (req, res, next) {
--- public/scripts.js
+++ public/scripts.js
@@ -1,59 +1,50 @@
-
-$( document ).ready(function() {
-	fetchBookmarks()
-
-});
-
-$('.url-submit').on('click', (e) => {
-  e.preventDefault();
-  console.log("hello");
-});
-
-$('.folder-submit').on('click', (e) => {
-  console.log("hello");
-});
-
-$(document).on('click', '.folder-listitem', function(e) {
-	var id = e.target.value
-	fetchFolder(id)
-})
-
-function fetchBookmarks () {
-  axios.get('/api/folders')
-  .then((response) => {
-debugger
-		response.data.map(function(folder) {
-			$('.folder-display').append(`<li>
-																		<button value=${folder.id}
-																				class='folder-listitem'>
-																				${folder.title}
-																					<span class="id">
-																						${folder.id}
-																					</span>
-																			</button>
-																		</li>`);
-			$('.folder-dropdown').append(`
-				<option id=${folder.id}>${folder.title}</option>
-				`);
-			})
-})
-  .catch(function(error) {
-  console.log('Error receiving bookmarks')
-})
+function showFolders(jsonData) {
+  $('.folders').append(`
+    <div class="folder" id=${jsonData.id}>
+      <p>${jsonData.folder_title}</p>
+    </div>
+  `)
+  $('.folder-dropdown').append(`
+    <option id=${jsonData.id}>${jsonData.folder_title}</option>
+    `)
 }
 
-function fetchFolder(id){
-	axios.get(`/api/folders/${id}`)
-	.then((response) => {
-		console.log(Object.values(response.data)[0].id, Object.values(response.data)[0].title )
-	})
-}
+$.get('/api/folders', function(data) {
+  for(var key in data) {
+    if (data.hasOwnProperty(key))
+    $('.folders').append(`
+      <div class="folder" id=${data[key].id}>
+        <p>${data[key].folder_title}</p>
+      </div>
+    `)
+    $('.folder-dropdown').append(`
+      <option id=${data[key].id}>${data[key].folder_title}</option>
+      `)
+  }
+})
 
-function mapId(){
-	// axios.get('/api/folders')
-	// .then((response) => {
-	// 	response.data.map(function(item) {
-	// 		return {item.id: item.title}
-	// 	})
-	// }
-}
+$('.add-folder-button').on('click', function(e) {
+  e.preventDefault()
+
+  $.ajax({
+    url: '/api/folders',
+    type: 'post',
+    data: {
+      folder: $('.add-folder-input').val()
+    },
+    success: showFolders
+  })
+})
+
+$('.add-url-button').on('click', function(e) {
+  e.preventDefault()
+
+  $.ajax({
+    url: '/api/urls',
+    type: 'post',
+    data: {
+      url: $('.add-url-input').val(),
+      folderId: $('option:selected').attr('id')
+    }
+  })
+})
