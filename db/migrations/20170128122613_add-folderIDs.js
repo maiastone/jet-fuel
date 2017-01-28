360ea025ec949b74365f32e7011d3541b77e4f75
Maia Stone <maiadov@gmail.com>
2017-01-28 14:41:48 -0700

    WIP fixing errors for heroku deploy

--- db/migrations/20170128122613_add-folderIDs.js
+++ db/migrations/20170128122613_add-folderIDs.js
@@ -1,15 +1,11 @@
 exports.up = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.table('urls', function(table){
+  return knex.schema.table('urls', function(table) {
       table.renameColumn('folder_folderID', 'folderID');
-    })
-  ])
+  })
 };
 
 exports.down = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.table('urls', function(table) {
+  return knex.schema.table('urls', function(table) {
     table.renameColumn('folderID', 'folder_folderID');
-    })
-  ])
+  })
 };
